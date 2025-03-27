66086372d43447aa737baf27fb60e872486eefcb
Alysson <alyssondavi.16@gmail.com>
2025-03-26 21:16:48 -0300

    alterações referente a comentários e correções de funções

--- criacaodalogica.c
+++ criacaodalogica.c
@@ -53,7 +53,7 @@ printf("Insira a quantidade de pontos turisticos: \n");
 printf("Pontos Turisticos: ");
 scanf(" %d", &pontosturisticos1); //%d para inserir um numero inteiro
 
-// Logo abaixo as variaveis densidade populacional e pib per capita que serao calculadas em tela
+// Logo abaixo as variaveis densidade populacional e pib per capita que serao calculadas em tela//
 
 densidadepopulacional1 =  populacao1 / area1;
 printf("Densidade Populacional e: %.2f\n", densidadepopulacional1);
@@ -101,31 +101,33 @@ printf("PIB per Capita: %.2f reais\n"), pibpercapita2;
 
 //Abaixo sera impresso informando as informações da primeira carta do jogo//
 
-printf("Estado: %c \n", estado1);
-printf("Código: %c%2s \n", estado1, codigo1);
-printf("Nome da cidade: %s\n",cidade1);
-printf("Polulação: %d\n", populacao1);
-printf("Área: %.3f\n", area1);
-printf("PIB: %.3f bilhões de reais\n", pib1);
-printf("Pontos turisticos: %d\n", pontosturisticos1);
-printf("Densidade populacional e: %.2f hab/km²\n", densidadepopulacional1);
-printf("PIB per capita e: %.2f reais/hab\n", pibpercapita1);
+printf("Estado: %c \n", estado1); //%c para inserir um caractere//
+printf("Código: %c%2s \n", estado1, codigo1); //%c para inserir um caractere e %2s para inserir uma string de 2 caracteres//
+printf("Nome da cidade: %s\n",cidade1); //%s para inserir uma string//
+printf("Polulação: %d\n", populacao1); //%d para inserir um numero inteiro//
+printf("Área: %.3f\n", area1); //%f para inserir um numero real//
+printf("PIB: %.3f bilhões de reais\n", pib1); //%f para inserir um numero real//
+printf("Pontos turisticos: %d\n", pontosturisticos1); //%d para inserir um numero inteiro//
+printf("Densidade populacional e: %.2f hab/km²\n", densidadepopulacional1); //%f para inserir um numero real//
+printf("PIB per capita e: %.2f reais/hab\n", pibpercapita1); //%f para inserir um numero real//
 
 
 // Em seguida abaixo estara a impressao informando a segunda carta do jogo//
 
-printf("Estado: %c \n", estado2);
-printf("Código: %c%2s \n", estado2, codigo2);
-printf("Nome da cidade: %s\n", cidade2);
-printf("Polulação: %d\n", populacao2);
-printf("Área: %.3f\n", area2);
-printf("PIB: %.3f bilhões de reais\n", pib2);
-printf("Pontos turisticos: %d\n", pontosturisticos2);
-printf("Densidade populacional e: %.2f hab/km²\n", densidadepopulacional2);
-printf("PIB per capita e: %.2f reais/hab\n", pibpercapita2);
+printf("Estado: %c \n", estado2); //%c para inserir um caractere// 
+printf("Código: %c%2s \n", estado2, codigo2); //%c para inserir um caractere e %2s para inserir uma string de 2 caracteres//
+printf("Nome da cidade: %s\n", cidade2); //%s para inserir uma string//
+printf("Polulação: %d\n", populacao2); //%d para inserir um numero inteiro//
+printf("Área: %.3f\n", area2); //%f para inserir um numero real//
+printf("PIB: %.3f bilhões de reais\n", pib2); //%f para inserir um numero real//
+printf("Pontos turisticos: %d\n", pontosturisticos2); //%d para inserir um numero inteiro//
+printf("Densidade populacional e: %.2f hab/km²\n", densidadepopulacional2); //%f para inserir um numero real//
+printf("PIB per capita e: %.2f reais/hab\n", pibpercapita2); //%f para inserir um numero real//
 
+// Abaixo sera impresso a informação de qual atributo foi utulizado para definir a carta vencedora//
 printf("Foi utilizado o atributo 'População' para defeinir a carta vencedora\n"); 
 
+// Abaixo sera impressão das cartas e suas respectivas populações//
 printf("Carta 1:  %s", cidade1), - printf(" %d\n", populacao1);
 printf("Carta 2:  %s", cidade2), - printf(" %d\n", populacao2);
 
